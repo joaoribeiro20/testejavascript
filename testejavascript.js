7a48be29ff681378a8bf5ee621a4bcc63edbb14b
João Ribeiro <jvalves217@gmail.com>
2023-01-27 13:40:09 -0300

    inicio

---
+++ testejavascript.js
@@ -0,0 +1,9 @@
+function resultado () {
+var elemento = document.getElementById("re");
+var text1 = document.getElementById("txt1").value;
+var text2 = document.getElementById("txt2").value;
+/*var valor1 = parseFloat(text1);
+var valor2 = parseFloat(text2);*/
+var soma = valor1 + valor2;
+elemento.innerHTML = soma;
+}
